97bad47c0ffcb1166bd61198b35f3143d3ae8e02
Tomasz Kowalczewski <tomasz.kowalczewski@codewise.com>
2015-10-11 10:57:22 +0200

    Global empty invalid tweet placeholder

---
+++ src/main/java/pl/tkowalcz/twitter/Tweets.java
@@ -0,0 +1,10 @@
+package pl.tkowalcz.twitter;
+
+public class Tweets {
+
+    public static Tweet EMPTY_INVALID = new Tweet();
+
+    public static Tweet emptyInvalidTweet() {
+        return EMPTY_INVALID;
+    }
+}
